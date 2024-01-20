920ee50664fc3b1ae87439e2298a4c67349e5db2
Matthew Clancy <mmclancy01@yahoo.com>
2024-01-20 16:52:49 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i-- ) {
+        console.log(i); 
+    }
+}
